85bc82842863f6a5fe35548b5f1b80d3acaefea9
edmund <edmund.pgh@gmail.com>
2013-01-21 15:28:21 -0800

    write to file; and get inputs from user

--- ladder.py
+++ ladder.py
@@ -1,11 +1,12 @@
 import math
 
-f_low = float(1e-13)
-f_high = float(1e13)
-phi = 15.0
-n = 50
+f_low = float(raw_input('f_low = '))
+f_high = float(raw_input('f_high = '))
+phi = float(raw_input('phi = '))
+n = int(raw_input('n = '))
 
-print '.subckt cpe in out'
+f = open('cpe.cir', 'w')
+print >> f, '.subckt cpe in out'
 
 r_1 = 1
 c_1 = 1 / f_low
@@ -16,10 +17,10 @@ assert b < 1
 assert a < 1
 
 for i in range(n):
-    print 'r%s in rung_%i %s' % (i, i, r_1 * pow(a, i))
-    print 'c%s rung_%i out %s' % (i, i, c_1 * pow(b, i))
+    print >> f, 'r%s in rung_%i %s' % (i, i, r_1 * pow(a, i))
+    print >> f, 'c%s rung_%i out %s' % (i, i, c_1 * pow(b, i))
 
-print 'rP in out %s' % (r_1 * ((1 - a) / a))
-print 'rC in out %s' % (c_1 * (pow(b, n) / (1 - b)))
+print >> f, 'rP in out %s' % (r_1 * ((1 - a) / a))
+print >> f, 'rC in out %s' % (c_1 * (pow(b, n) / (1 - b)))
 
-print '.ends'
+print >> f, '.ends'
