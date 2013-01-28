2c35c5979a851de0fa3f2830c1988d032dd0f442
Edmund Huber <edmund@nang.(none)>
2013-01-27 18:04:24 -0800

    cpe that works with a=0.1,0.5,0.9, d_p=1e4

--- ladder.py
+++ ladder.py
@@ -1,38 +1,30 @@
 import math
 import numpy
 
-phi = float(raw_input('phi = '))
-n = int(raw_input('n = '))
-r_1 = float(raw_input('r_1 = '))
-c_1 = float(raw_input('c_1 = '))
+alpha = float(raw_input('alpha = '))
+n = 1000 #int(raw_input('n = '))
 d_p = float(raw_input('d_p = '))
+cpe = str(raw_input('name ='))
 
-f = open('cpe.cir', 'w')
-print >> f, '.subckt cpe in out'
-
-ab = 0.24 / (1 + 0.5)
+f_low = 1e-6
+f_high = 1e12
+pi = math.pi
+phi = 90 * alpha
+r_1 = 1 / f_low
+c_1 = 1 
+ab = pow(f_low / f_high, 1.0 / n)
 a = pow(10, (phi / 90.0) * math.log(ab, 10))
 b = ab / a
-print 'a = %s' % a
-print 'b = %s' % b
 
 r_k = [r_1] + [r_1 * pow(a, k + 1) for k in range(n - 1)]
 c_k = [c_1] + [c_1 * pow(b, k + 1) for k in range(n - 1)]
-print 'r_k = %s' % r_k
-print 'c_k = %s' % c_k
 
 r_p = r_1 * ((1 - a) / a)
 c_p = c_1 * pow(b, n) / (1 - b)
-w_av = pow(a / b, 0.25) / (r_1 * c_1 * pow(ab, 2))
-y_rungs = sum((1j * w_av * c_k[k]) / (1 + (1j * w_av * r_k[k] * c_k[k])) for k in range(n))
-z_av = 1.0 / numpy.real((1.0 / r_p) + (1j * w_av * c_p) + y_rungs)
-d = z_av * pow(w_av, phi / 90.0)
-print 'r_p = %s, c_p = %s' % (r_p, c_p)
-print 'w_av = %s' % w_av
-print 'z_av = %s' % z_av
-print 'd = %s' % d
 
-fixup = d_p / d
+y = 1/r_p + 2 * pi * 1j * c_p + sum((1j * 2 * pi * c_k[k])/(1+1j*r_k[k]* 2 * pi * c_k[k]) for k in range (n))
+z = abs(1 / y)
+fixup = d_p / z
 print 'fixup = %s' % fixup
 
 r_p *= fixup
@@ -42,6 +34,12 @@ c_k = map(lambda c: c / fixup, c_k)
 print 'fixed r_p = %s, c_p = %s' % (r_p, c_p)
 print 'fixed r_k = %s' % r_k
 print 'fixed c_k = %s' % c_k
+
+# Output circuit.
+
+f = open('%s.cir' % cpe, 'w')
+print >> f, '.subckt '+ cpe + ' in out'
+print >> f, '* ' + 'alpha= ' + '%s' %alpha + ',n= ' + '%s' %n + ',flow= ' + '%s' %f_low + ',fhigh= ' + '%s' %f_high + ',D= ' + '%s' %d_p
 
 i = 0
 for r, c in zip(r_k, c_k):
